188ace5a61370164e038dc0897fdf5bef7c3c23c
Brandon Istfan <brandonistfan@gmail.com>
2024-03-23 22:10:48 -0400

    grade updates

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 import {gradeUpdate} from "./grade_update.js";
 
 let failCounter = 0;
-await gradeUpdate('REYUvIEh0Mc2zEjdvYRs', 'eBoTg6SI96sbRCB1Qcvv', failCounter);
+await gradeUpdate('REYUvIEh0Mc2zEjdvYRs', 'DVJU8r8HDAKxGFkmjV0O', failCounter);
+await gradeUpdate('REYUvIEh0Mc2zEjdvYRs', 'tbFSjnoKFztCjXMPBGRC', failCounter);
+await gradeUpdate('REYUvIEh0Mc2zEjdvYRs', 'vvEAOm1vEHJmJdiX3g42', failCounter);
 
 process.exit();
