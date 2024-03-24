77181a7201bc3a5499e7d575e5353b0e867ada03
Krish Mehta <me@krishvmehta.com>
2024-03-24 03:11:12 -0400

    Integrate grade update function into cloud function

--- functions/index.js
+++ functions/index.js
@@ -1,10 +1,11 @@
 const functions = require('firebase-functions');
 const admin = require('firebase-admin');
+const {gradeUpdate} = require("../grade_update");
 admin.initializeApp();
 
 exports.processNewAssignment = functions.firestore
     .document('assignments/{assignmentId}')
     .onWrite(async (snap, context) => {
         const assignmentID = context.params.assignmentId;
-        console.log(assignmentID)
+        await gradeUpdate(assignmentID)
     });
