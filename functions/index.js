65683e244c794219003624715646972a75be3a6b
Brandon Istfan <brandonistfan@gmail.com>
2024-03-24 00:26:35 -0400

    Firebase Initialization

--- functions/index.js
+++ functions/index.js
@@ -1,19 +1,11 @@
-/**
- * Import function triggers from their respective submodules:
- *
- * const {onCall} = require("firebase-functions/v2/https");
- * const {onDocumentWritten} = require("firebase-functions/v2/firestore");
- *
- * See a full list of supported triggers at https://firebase.google.com/docs/functions
- */
+const functions = require('firebase-functions');
+const admin = require('firebase-admin');
+admin.initializeApp();
 
-const {onRequest} = require("firebase-functions/v2/https");
-const logger = require("firebase-functions/logger");
-
-// Create and deploy your first functions
-// https://firebase.google.com/docs/functions/get-started
-
-// exports.helloWorld = onRequest((request, response) => {
-//   logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+exports.gradeAssignments = functions.firestore
+    .document('assignments/{assignmentId}')
+    .onCreate((snap, context) => {
+        const newAssignment = snap.data();
+        console.log('New assignment uploaded:', newAssignment);
+        return snap.ref.set({ graded: true }, { merge: true });
+    });
